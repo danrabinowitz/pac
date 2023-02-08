48557c9f14c59ee36ec034b6cdfad5ab9c753b22
Dan Rabinowitz <djr@danielrabinowitz.com>
2023-02-07 17:11:13 -0700

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,3 +1,3 @@
 function FindProxyForURL(url, host) {
-  return "HTTP proxy.djrtechconsulting.com:3128; HTTPS proxy.djrtechconsulting.com:3128";
+  return "HTTP 192.168.5.16:3128; HTTPS 192.168.5.16:3128";
 }
